75626c2a278b4785b149321ba116a59a115bd058
adi-mws <aditya040mth@gmail.com>
2026-01-02 16:23:20 +0530

    Learnt about the app router...client and server components the edge + server architecture of next.js

---
+++ next-crud-app/app/auth/login/page.jsx
@@ -0,0 +1,11 @@
+import { Box, Button } from '@mui/material'
+import React from 'react'
+
+export default function LoginPage() {
+  return (
+    <Box>
+        <Button variant='contained'>Login Now</Button>
+
+    </Box>
+  )
+}
--- next-crud-app/app/blogs/layout.jsx
+++ next-crud-app/app/blogs/layout.jsx
@@ -1,14 +1,17 @@
 "use client"
 
 import { Box, Button, Typography } from "@mui/material"
+import { useRouter } from "next/navigation"
 
 export default function Blogs({ children }) {
+  const router = useRouter(); 
+  
   return (
     <Box>
 
       <Typography variant="outlined"> This is the blog layout (Stable)</Typography>
       {children}
-      <Button variant="contained">Blog Layout Footer</Button>
+      <Button variant="contained" onClick={() => router.push('/login')}>Blog Layout Footer</Button>
     </Box>
   )
 }
